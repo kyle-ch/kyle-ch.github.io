717bd066520cc52fb68dcb9320273e4724e76d34
Kyle Chan <kc.kylechan@gmail.com>
2018-03-19 18:15:11 -0400

    Removed google analytics

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -5,7 +5,7 @@ class AboutMe extends Component {
   renderAboutMeBody() {
     const aboutMe = `I'm a full stack software engineer with experience in Ruby
     on Rails and React! I was born and raised in NYC but now I'm in Boston in my
-    3rd year at Northeastern Universityworking in my Bachelor's degree in
+    3rd year at Northeastern University working on my Bachelor's degree in
     computer science and finance. Right now, I am a full stack engineer co-op at
     Forward Financing. I have also done a previous co-op at Jobcase.`
     const interests = `Talk to me about cars, food, the latest streetwear drops,
