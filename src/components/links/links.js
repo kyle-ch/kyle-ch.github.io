ace49a1e69c5bed21100d85f94557785c758b0ae
Kyle Chan <kc.kylechan@gmail.com>
2018-06-25 11:53:03 -0400

    Bug fix

--- src/components/links/links.js
+++ src/components/links/links.js
@@ -6,9 +6,9 @@ class Links extends Component {
     return (
         <div className="links breadcrumb is-centered" aria-label="breadcrumbs">
           <ul>
-            <li><a href="https://github.com/kyle-ch" target="_blank"><span className="icon is-small"><i className="fab fa-github"></i></span></a></li>
-            <li><a href={Resume} target="_blank"><span className="icon is-small"><i className="fas fa-file-alt"></i></span></a></li>
-            <li><a href="https://www.linkedin.com/in/kyle-ch" target="_blank"><span className="icon is-small"><i className="fab fa-linkedin"></i></span></a></li>
+            <li><a href="https://github.com/kyle-ch" target="_blank" rel="noopener noreferrer"><span className="icon is-small"><i className="fab fa-github"></i></span></a></li>
+            <li><a href={Resume} target="_blank" rel="noopener noreferrer"><span className="icon is-small"><i className="fas fa-file-alt"></i></span></a></li>
+            <li><a href="https://www.linkedin.com/in/kyle-ch" target="_blank" rel="noopener noreferrer"><span className="icon is-small"><i className="fab fa-linkedin"></i></span></a></li>
           </ul>
         </div>
     );
