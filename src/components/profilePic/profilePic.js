548cb0ef6eb9d00bf8cf940e6784f094fdf8066a
Kyle <kc.kylechan@gmail.com>
2019-03-16 11:55:03 -0400

    Change pic

--- src/components/profilePic/profilePic.js
+++ src/components/profilePic/profilePic.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import profilePicture from './profilePictureV2.jpg'
+import profilePicture from './profilePicture.jpeg'
 import './profilePic.css';
 
 class ProfilePic extends Component {
